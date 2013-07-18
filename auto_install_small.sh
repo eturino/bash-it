25044439cd67d39fa8ad35d9274e142951c2bb30
Eduardo Turiño <eturino@gmail.com>
2013-07-18 18:11:03 +0200

    fix autoconfig, filetype

--- auto_install_small.sh
+++ auto_install_small.sh
@@ -22,7 +22,7 @@ function load_small() {
   for name in "${argAry2[@]}"
   do
     echo "Enabling $file_type $name"
-    src="$BASH_IT/$file_type/available/${name}.aliases.bash"
+    src="$BASH_IT/$file_type/available/${name}.${file_type}.bash"
     filename="$(basename ${src})"
     [ ${filename:0:1} = "_" ] && continue
       dest="${BASH_IT}/${file_type}/enabled/${filename}"
